236e43d695407fe0a66ed88a69d6cb2186318aa3
David Orme <david.orme@rallyhealth.com>
2018-09-10 21:01:25 -0500

    First cut: fetch-ns

--- src/clj/boot_code/repl.clj
+++ src/clj/boot_code/repl.clj
@@ -1,5 +1,12 @@
-(ns boot-code.repl)
+(ns boot-code.repl
+  (:require [clojure.java.io :as io]
+            [boot-code.api :as api]))
 
 
 (defn fetch-ns [path]
-  (println (str "Not implemented: fetch " path)))
+  (let [full-path (str (:full-name (api/file-details ".")) "/src/cljs/" path)]
+    (println (str "[FETCH] " full-path))
+    (when-let [content (slurp full-path)]
+      {:status 200
+       :headers {"Content-Type" "text/plain"}
+       :body content})))
