5faffce076eb29d26f60e0da5c61a2718679d616
David Orme <david.orme@rallyhealth.com>
2018-09-27 23:11:16 -0500

    Resolve all cljs on Fusion classpath

--- src/clj/boot_code/repl.clj
+++ src/clj/boot_code/repl.clj
@@ -1,13 +1,38 @@
 (ns boot-code.repl
   (:require [clojure.java.io :as io]
+            [clojure.java.classpath :as cp]
             [boot-code.api :as api]
-            [boot-code.io :refer [file-details]]))
+            [boot-code.io :refer [file-details]])
+  (:import [java.io File]
+           [java.util.jar JarFile JarEntry]))
+
+
+;; TODO: In a Boot plugin I don't think these are necessary either.  But maybe for Scala?
+(defn- classpath-str->seq
+  "Translate a classpath string to a sequence of classpath entries, respecting the
+  platform path separator.  Returns an empty sequence if called with nil."
+  [classpath]
+  (if classpath (.split classpath (System/getProperty "path.separator")) []))
+
+
+(defn- classpath-objs
+  "Return the classpath as a map {:dirs [^File] :jars [^JarFile]} where each classpath
+  entry is grouped/classified by if it is a directory or a Jar file."
+  [classpath-seq]
+  (->> classpath-seq
+       (map #(File. ^String %))
+       (map #(if (cp/jar-file? %) (JarFile. %) %))
+       (group-by
+        (fn [f]
+          (cond
+            (instance? JarFile f) :jars
+            (.isDirectory f)      :dirs
+            :else                 :resources)))))
 
 
 (defn fetch-ns [path]
-  (let [full-path (str (:full-name (file-details ".")) "/src/cljs/" path)]
-    (println (str "[FETCH] " full-path))
-    (when-let [content (slurp full-path)]
-      {:status 200
-       :headers {"Content-Type" "text/plain"}
-       :body content})))
+  (println (str "[FETCH] " path))
+  (when-let [content (slurp (io/resource path))]
+    {:status 200
+     :headers {"Content-Type" "text/plain"}
+     :body content}))
